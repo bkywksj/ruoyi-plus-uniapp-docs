ac4d6c966476fedd312af2e0314519da93cc1e33
抓蛙师 <770492966@qq.com>
2025-08-03 00:21:16 +0800

    新增样式文件，添加主题颜色和侧边栏背景色变量

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -3,6 +3,7 @@ import { h } from 'vue'
 import DefaultTheme from 'vitepress/theme'
 import type { Theme } from 'vitepress'
 import PreviewFrame from './components/PreviewFrame.vue'
+import './style.css'
 
 export default {
     extends: DefaultTheme,
